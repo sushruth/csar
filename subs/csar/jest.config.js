ed468823fee7fb95a5ecc1d37b50552a106dfd13
Sushruth Sastry <shastry.sushruth@gmail.com>
2022-08-19 15:59:47 -0700

    Regular upgrades

--- subs/csar/jest.config.js
+++ subs/csar/jest.config.js
@@ -1,12 +1,20 @@
-/** @type {import('@jest/types').Config.InitialOptions} */
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  transform: {
-    "^.+\\.tsx?$": "esbuild-jest",
+  preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.spec.json',
+    },
   },
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
   collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/*.types.ts"],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.types.ts',
+    '!src/index.ts',
+  ],
+  coverageReporters: ['json', 'html'],
   clearMocks: true,
-  setupFilesAfterEnv: ["@testing-library/jest-dom"],
-  testEnvironment: "jsdom"
-};
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  testEnvironment: 'jsdom',
+}
